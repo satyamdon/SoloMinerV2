3f964489d13a238b1614ab85aaac7d8e5e381ef6
satyamdon <nirajmehta462@gmail.com>
2025-03-13 10:32:40 +0530

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = 'bc1qnyp9rscwwax5l60jr8k4l2mq9x3wwycyj5j63c' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
